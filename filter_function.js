904379c06f803f6d5cb9fda86daeb1c31380e47f
thanse2s <tohan0510@gmail.com>
2025-02-11 16:12:40 +0100

    add function createStartTable

--- filter_function.js
+++ filter_function.js
@@ -3,3 +3,32 @@ console.log(pokemon)
 
 var tableBody = document.getElementById("tableBody");
 tableBody.innerHTML=pokemon
+
+function createStartTable(){
+
+    const table = document.createElement('table');
+    table.setAttribute('border','1');
+
+    const headerRow = document.createElement('tr');
+    Object.keys(pokemon[0]).forEach(key=>{
+        const th = document.createElement('th');
+        th.appendChild(document.createTextNode(key));
+        headerRow.appendChild(th);
+    })
+    table.appendChild(headerRow);
+
+    pokemon.forEach(item=>{
+        const row = document.createElement('tr');
+        Object.values(item).forEach(value=>{
+            const td=document.createElement('td');
+            td.appendChild(document.createTextNode(value));
+            row.appendChild(td);
+        });
+        table.appendChild(row);
+    });
+
+    const tablebody = document.getElementById('tableBody');
+    tableBody.appendChild(table);
+}
+
+createStartTable();
