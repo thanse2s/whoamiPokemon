000bc2fbb6fa0275248eea7d64bc408b8e256c4a
thanse2s <tohan0510@gmail.com>
2025-02-16 00:13:28 +0100

    Edit filter.js

--- filter.js
+++ filter.js
@@ -3,5 +3,11 @@ import {pokemonData} from "./pokemon.js";
 const maxSize = Math.max(pokemonData.map(poke => poke.Hight));
 const maxWeight = Math.max(pokemonData.map(poke => poke.Gewicht));
 
-document.getElementById("max-weight").value=maxWeight;
-document.getElementById("max-height").value=maxSize;
+
+console.log(maxWeight);
+console.log(maxWeight);
+console.log("HelloWorld!");
+console(pokemonData);
+
+document.getElementById("max-weight").value = maxWeight;
+document.getElementById("max-height").value = maxSize;
