6de046d292e74c0febfd0c574133fdd6ad86a9dd
thanse2s <tohan0510@gmail.com>
2025-02-16 04:17:27 +0100

    Edit filter.js

--- filter.js
+++ filter.js
@@ -5,8 +5,12 @@ const maxWeight = Math.max(...pokemon.map(poke => parseFloat(poke.Gewicht)));
 const minSize = Math.min(...pokemon.map(poke => parseFloat(poke.Hight)));
 const minWeight = Math.min(...pokemon.map(poke => parseFloat(poke.Gewicht)));
 
+console.log(minSize);
+console.log(minWeight);
+
 //Set max an min Value Weight and Size
 document.getElementById("max-weight").value = maxWeight;
 document.getElementById("max-height").value = maxSize;
 document.getElementById("min-weight").value = minWeight;
 document.getElementById("min-height").value = minSize;
+
