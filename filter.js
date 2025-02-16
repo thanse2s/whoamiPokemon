07a28382cfb72dd281e57f07ef3ba4b1b63a0f91
thanse2s <tohan0510@gmail.com>
2025-02-16 04:06:06 +0100

    Edit pokemon.js

--- filter.js
+++ filter.js
@@ -2,12 +2,11 @@ import { pokemon } from "./pokemon.js";
 
 const maxSize = Math.max(...pokemon.map(poke => parseFloat(poke.Hight)));
 const maxWeight = Math.max(...pokemon.map(poke => parseFloat(poke.Gewicht)));
+const minSize = Math.min(...pokemon.map(poke => parseFloat(poke.Hight)));
+const minWeight = Math.min(...pokemon.map(poke => parseFloat(poke.Gewicht)));
 
-
-console.log(maxWeight);
-console.log(maxWeight);
-console.log("HelloWorld!");
-console.log(pokemon);
-
+//Set max an min Value Weight and Size
 document.getElementById("max-weight").value = maxWeight;
 document.getElementById("max-height").value = maxSize;
+document.getElementById("min-weight").value = minWeight;
+document.getElementById("min-height").value = minSize;
