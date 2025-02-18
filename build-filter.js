c528cc777a22467841982ef66b5681391deee1c9
thanse2s <tohan0510@gmail.com>
2025-02-18 22:31:25 +0100

    Add index3.html & build-filter.js

---
+++ build-filter.js
@@ -0,0 +1,14 @@
+const types = ['fire','water','leaf','bug','dark','dragon','electric','fariy','fight','flying','ghost','ground','ice','normal','poison','psycho','rock','steel']
+
+
+function createTypeFilter(){
+
+
+
+
+
+    document.getElementById("type-panel-body").innerHTML = "TESTING";
+}
+
+
+createTypeFilter();
