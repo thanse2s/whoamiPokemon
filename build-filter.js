0bffb77eb444c53e588a77eff0daad61c24f0b37
thanse2s <tohan0510@gmail.com>
2025-02-18 23:02:25 +0100

    Edit index3.html

--- build-filter.js
+++ build-filter.js
@@ -7,18 +7,18 @@ function createTypeFilter(){
     types.forEach(type => {
         filterHTML += `<!--${type}-->`
         filterHTML += `<div class="row">`
+            filterHTML += `<div class="col">`
+                filterHTML += `${type}`
+            filterHTML += `</div>`
         filterHTML += `<div class="col">`
-        filterHTML += `${type}`
-        filterHTML += `</div>`
-        filterHTML += `<div class="col">`
-        filterHTML += `<div class="btn-group" role="group">`
-        filterHTML += `<input type="radio" class="btn-check" name="${type}-radio" id="${type}-false" autocomplete="off">`
-        filterHTML += `<label class="btn btn-outline-danger" for="${type}-false"> &#10008; </label>`
-        filterHTML += `<input type="radio" class="btn-check" name="${type}-radio" id="${type}-neutral" autocomplete="off" checked>`
-        filterHTML += `<label class="btn btn-outline-secondary" for="${type}-neutral"> &minus; </label>`
-        filterHTML += `<input type="radio" class="btn-check" name="${type}-radio" id="${type}-true" autocomplete="off">`
-        filterHTML += `<label class="btn btn-outline-success" for="${type}-true"> &#10003; </label>`
-        filterHTML += `</div>`
+            filterHTML += `<div class="btn-group" role="group">`
+                filterHTML += `<input type="radio" class="btn-check" name="${type}-radio" id="${type}-false" autocomplete="off">`
+                filterHTML += `<label class="btn btn-outline-danger" for="${type}-false"> &#10008; </label>`
+                filterHTML += `<input type="radio" class="btn-check" name="${type}-radio" id="${type}-neutral" autocomplete="off" checked>`
+                filterHTML += `<label class="btn btn-outline-secondary" for="${type}-neutral"> &minus; </label>`
+                filterHTML += `<input type="radio" class="btn-check" name="${type}-radio" id="${type}-true" autocomplete="off">`
+                filterHTML += `<label class="btn btn-outline-success" for="${type}-true"> &#10003; </label>`
+            filterHTML += `</div>`
         filterHTML += `</div>`
     })
     filterHTML += `</div>`
