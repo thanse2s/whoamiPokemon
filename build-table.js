ee72036b19e92e1ff661cf9ba985003ca7e54d0e
thanse2s <tohan0510@gmail.com>
2025-02-19 19:04:21 +0100

    Edit build-filter.js

--- build-table.js
+++ build-table.js
@@ -2,11 +2,27 @@ import { pokemonList } from "./pokemonlist.js";
 
 function createFullList(){
 
-    let html=``;
-    pokemonList.forEach(pokemon=>{
-
+    let html=`<table class="table table-striped table-dark">`;
+    html+=`<thead>`;
+    html+=`<tr>`;
+    Object.keys(pokemonList[0]).forEach((key) => {
+       html+=`<th scope="col">${key}</th>`;
     });
-    document.getElementById("full-liste").innerHTML=html;
+    html+=`</tr>`;
+    html+=`</thead>`;
+    html+=`<tbody>`;
+    pokemonList.forEach((element) => {
+        html+=`<tr>`;
+        Object.values(element).forEach((value) => {
+            html+=`<td>${value}</td>`;
+        })
+        html+=`</tr>`;
+    })
+    html+=`</tbody></table>`;
+
+
+
+    document.getElementById("full_list").innerHTML=html;
 
 }
 
