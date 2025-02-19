89d80795021b36788264b63c99a7837abada5f81
thanse2s <tohan0510@gmail.com>
2025-02-19 10:16:12 +0100

    Edit index3.html & build-filter.js

--- build-filter.js
+++ build-filter.js
@@ -1,9 +1,12 @@
 const types = ['fire','water','leaf','bug','dark','dragon','electric','fariy','fight','flying','ghost','ground','ice','normal','poison','psycho','rock','steel']
+const evp_types = ['levelup','trade','move','friendship','item']
+const evo = ['pre_evo','post_evo']
 
 
 function createTypeFilter(){
 
-    let filterHTML = `<div class="container text-center">`
+    let filterHTML = ``
+    filterHTML +=`<div class="container">`
     types.forEach(type => {
         filterHTML += `<!--${type}-->`
         filterHTML += `<div class="row">`
@@ -19,7 +22,7 @@ function createTypeFilter(){
                 filterHTML += `<input type="radio" class="btn-check" name="${type}-radio" id="${type}-true" autocomplete="off">`
                 filterHTML += `<label class="btn btn-outline-success" for="${type}-true"> &#10003; </label>`
             filterHTML += `</div>`
-        filterHTML += `</div> <br>`
+        filterHTML += `</div>`
     });
     filterHTML += `</div>`
 
@@ -27,5 +30,31 @@ function createTypeFilter(){
     document.getElementById("type-panel-body").innerHTML =filterHTML;
 }
 
+function createEvoFilter(){
 
+    let filterHTML=``;
+    filterHTML +=`<div class="container">`
+    evo.forEach(type => {
+        filterHTML += `<!--${type}-->`
+        filterHTML += `<div class="row">`
+        filterHTML += `<div class="col">`
+            filterHTML += `${type}:`
+        filterHTML += `</div>`
+        filterHTML += `<div class="col">`
+            filterHTML += `<div class="btn-group" role="group">`
+                filterHTML += `<input type="radio" class="btn-check" name="${type}-radio" id="${type}-false" autocomplete="off">`
+                filterHTML += `<label class="btn btn-outline-danger" for="${type}-false"> &#10008; </label>`
+                filterHTML += `<input type="radio" class="btn-check" name="${type}-radio" id="${type}-neutral" autocomplete="off" checked>`
+                filterHTML += `<label class="btn btn-outline-secondary" for="${type}-neutral"> &minus; </label>`
+                filterHTML += `<input type="radio" class="btn-check" name="${type}-radio" id="${type}-true" autocomplete="off">`
+                filterHTML += `<label class="btn btn-outline-success" for="${type}-true"> &#10003; </label>`
+            filterHTML += `</div>`
+        filterHTML += `</div>`
+    });
+    filterHTML += `</div>`
+
+    document.getElementById("evo-panel-body").innterHTML=filterHTML;
+}
+
+createEvoFilter();
 createTypeFilter();
