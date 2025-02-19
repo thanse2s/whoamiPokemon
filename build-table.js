587fedb29daedbb5ce50c02695fbc511737d46c2
thanse2s <tohan0510@gmail.com>
2025-02-19 16:32:41 +0100

    Edit build-filter.js

---
+++ build-table.js
@@ -0,0 +1,13 @@
+import { pokemonList } from "./pokemonlist.js";
+
+function createFullList(){
+
+    let html=``;
+    pokemonList.forEach(pokemon=>{
+
+    });
+    document.getElementById("full-liste").innerHTML=html;
+
+}
+
+createFullList()
